eebea48a59aafec9161cb795781f3909add76ed3
shenxin <sxddup@gmail.com>
2018-10-24 15:18:31 +0800

    修改拼写错误

---
+++ ConstraintTriangles/ConstraintTriangles/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConstraintTriangles
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.rb_AutoTriangle.Checked = true;
+            this.rb_ManualTriangle.Checked = false;
+        }
+    }
+}
---
+++ ConstraintTriangles/ConstraintTriangles/MainformPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstraintTriangles
+{
+    //////主窗体伙伴类，用到的部分结构体与内置方法
+    partial class MainForm
+    {
+
+    }
+}
