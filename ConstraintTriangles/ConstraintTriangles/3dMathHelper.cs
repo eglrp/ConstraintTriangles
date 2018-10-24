0d325b968fbfa67642d32ee097c88f53f0a6fb87
shenxin <sxddup@gmail.com>
2018-10-24 17:01:46 +0800

    新建的文件使用commit-am会提示需要先add

---
+++ ConstraintTriangles/ConstraintTriangles/3dMathHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/////三维用到的一些数学方法
+namespace ConstraintTriangles
+{
+    class _3dMathHelper
+    {
+
+    }
+}
